4b3bd7d08fda8133e122ca9836fab48c3305a7fc
unknown <CharanKumarKharadeS>
2024-08-07 13:43:33 +0530

    learnt adding images and event handling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "./App.css";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
-
+import { blog } from "./Data/blog.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp, faAmazonPay } from "@fortawesome/free-brands-svg-icons";
 function App() {
   let headerInfo = {
     email: "charan@gmail.com",
     phone: "909209091",
   };
+  let display = () => {
+    alert("WORKING TO REACTJS");
+  };
+  let addData = (a, b) => {
+    alert(a + b);
+  };
   return (
     <div className="App">
-      <Header headerInfo={headerInfo} cname="Charles" />;
-      <Container fluid>
+      <Header headerInfo={headerInfo} cname="Charles">
+        <h3>Im from APP JS</h3>
+        <FontAwesomeIcon
+          icon={faWhatsapp}
+          className="text-green"
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faAmazonPay}></FontAwesomeIcon>
+      </Header>
+      <button
+        onClick={() => {
+          addData(10, 20);
+        }}
+      >
+        Add Data
+      </button>
+      <br></br>
+      <button onClick={display}> Click me</button>
+      <p>IM INSIDE APP</p>
+      {/* <Container fluid>
         <Container>
           <Row>
             <Col className="col-12 text-center py-4 mx-4">
@@ -83,9 +108,32 @@ function App() {
             </Col>
           </Row>
         </Container>
+      </Container> */}
+      <Container>
+        <Row>
+          {blog.map((v, i) => {
+            return <ProductItems pitems={v} key={i} />;
+          })}
+        </Row>
       </Container>
       <Footer />;
     </div>
   );
 }
 export default App;
+
+function ProductItems({ pitems }) {
+  return (
+    <div className=" col-lg-3 mb-4">
+      <Card>
+        <Card.Body>
+          <Card.Title>{pitems.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">BCOM</Card.Subtitle>
+          <Card.Text>{pitems.body}</Card.Text>
+          <Card.Link href="#">{pitems.id}</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,18 @@
-function Header(props) {
-  console.log(props.headerInfo);
+function Header({ headerInfo, cname, children }) {
   return (
     <>
-      <h2>WElcome to Heading!!!!</h2>;
+      <h2>WElcome to Heading!!!!</h2>;{children}
       <p>
-        Hi,{props.headerInfo.phone}
+        Hi,{headerInfo.email}
+        {headerInfo.phone}
         <br></br>
       </p>
-      ;<p>{props.cname}</p>
+      ;<p>{cname}</p>
       <div className="name">
         <h3>hey</h3>
         <p>I'm a ReactJS Developer</p>
         <p>I'm from India</p>
-        <p>I have {20 + 2} years of experience</p>
+        <p>I have {0.1 + 0} years of experience</p>
       </div>
     </>
   );
