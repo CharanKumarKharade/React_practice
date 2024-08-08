e78b6f8c13692c57eabea5eb9fe7761d35b55297
unknown <CharanKumarKharadeS>
2024-08-08 18:34:07 +0530

    learnt state management and modal

--- src/App.js
+++ src/App.js
@@ -1,139 +1,89 @@
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+// import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "./App.css";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
-import { blog } from "./Data/blog.jsx";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWhatsapp, faAmazonPay } from "@fortawesome/free-brands-svg-icons";
+// import { blog } from "./Data/blog.jsx";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faWhatsapp, faAmazonPay } from "@fortawesome/free-brands-svg-icons";
+import Navbar from "./Navbar.jsx";
+import btncss from "./Button.module.css";
+import { useState } from "react";
 function App() {
-  let headerInfo = {
-    email: "charan@gmail.com",
-    phone: "909209091",
+  const display = () => {
+    setCount(1 + count);
   };
-  let display = () => {
-    alert("WORKING TO REACTJS");
-  };
-  let addData = (a, b) => {
-    alert(a + b);
-  };
-  return (
-    <div className="App">
-      <Header headerInfo={headerInfo} cname="Charles">
-        <h3>Im from APP JS</h3>
-        <FontAwesomeIcon
-          icon={faWhatsapp}
-          className="text-green"
-        ></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faAmazonPay}></FontAwesomeIcon>
-      </Header>
+
+  let [pshow, setpshow] = useState(false);
+  let template = "";
+  if (pshow) {
+    template = (
+      <>
+        <button
+          onClick={() => {
+            setpshow(false);
+          }}
+          style={{ backgroundColor: "red", margin: "20px", padding: "20px" }}
+        >
+          Hide
+        </button>
+        <h1>Hello World!</h1>
+      </>
+    );
+  } else {
+    template = (
       <button
         onClick={() => {
-          addData(10, 20);
+          setpshow(true);
+        }}
+        style={{ backgroundColor: "blue", margin: "20px", padding: "20px" }}
+      >
+        Show
+      </button>
+    );
+  }
+  let [count, setCount] = useState(100);
+  let [pstatus, setPstatus] = useState(false);
+  let [menuStatus, setmenuStatus] = useState(false);
+  return (
+    <div className="App">
+      <button className={btncss.en}>Enquire</button>
+      <div className={btncss.modal}> </div>
+      <div className={btncss.modalcss}>
+        <h3>Enquiry Now</h3>
+      </div>
+      <button
+        classname={btncss.micon}
+        onClick={() => {
+          setmenuStatus(!menuStatus);
         }}
       >
-        Add Data
+        &#9776;
       </button>
-      <br></br>
-      <button onClick={display}> Click me</button>
-      <p>IM INSIDE APP</p>
-      {/* <Container fluid>
-        <Container>
-          <Row>
-            <Col className="col-12 text-center py-4 mx-4">
-              <h1>Our Courses</h1>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="3" md="6">
-              <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Title>Course1</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">BA</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-              </Card>
-              <br></br>
-            </Col>
-            <Col lg="3" md="6">
-              <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Title>Course2</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">BCA</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-              </Card>
-              <br></br>
-            </Col>
-            <Col lg="3" md="6">
-              <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Title>Course3</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">BBA</Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-              </Card>
-              <br></br>
-            </Col>
-            <Col lg="3" md="6">
-              <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Title>Course4</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    BCOM
-                  </Card.Subtitle>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Card.Link href="#">Card Link</Card.Link>
-                  <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </Container> */}
-      <Container>
-        <Row>
-          {blog.map((v, i) => {
-            return <ProductItems pitems={v} key={i} />;
-          })}
-        </Row>
-      </Container>
-      <Footer />;
+      <div className={`btncss.menu ${menuStatus ? "btncss.activeMenu" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Courses</li>
+          <li>Gallery</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <input type={pstatus ? "text" : "password"} />
+      <button
+        onClick={() => {
+          setPstatus(!pstatus);
+        }}
+      >
+        {!pstatus ? "show" : "hide"}
+      </button>
+      {template}
+      <Header />
+      <button className={btncss.error}>Error</button>
+      <Navbar />
+      <button onClick={display}> Click Me {count}</button>
+      <Footer />
+      <button className={btncss.warning}>YOU AGAIn</button>
     </div>
   );
 }
 export default App;
-
-function ProductItems({ pitems }) {
-  return (
-    <div className=" col-lg-3 mb-4">
-      <Card>
-        <Card.Body>
-          <Card.Title>{pitems.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">BCOM</Card.Subtitle>
-          <Card.Text>{pitems.body}</Card.Text>
-          <Card.Link href="#">{pitems.id}</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,16 @@
-function Header({ headerInfo, cname, children }) {
+// function Header({ headerInfo, cname, children })
+import { useState } from "react";
+function Header() {
+  const [name, setName] = useState("Charles");
+  const change = () => {
+    setName("Devaraj");
+    console.log("name changed");
+  };
   return (
     <>
-      <h2>WElcome to Heading!!!!</h2>;{children}
-      <p>
-        Hi,{headerInfo.email}
-        {headerInfo.phone}
-        <br></br>
-      </p>
-      ;<p>{cname}</p>
-      <div className="name">
-        <h3>hey</h3>
+      <h2>WElcome to Heading!!!!</h2>;
+      <div>
+        <h3 onClick={change}>hey,{name} from India</h3>
         <p>I'm a ReactJS Developer</p>
         <p>I'm from India</p>
         <p>I have {0.1 + 0} years of experience</p>
